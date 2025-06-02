3a39732b71238ac1ceaa1d5616547502243316b2
okolie emmanuel <Leoemmanuel93@gmail.com>
2025-06-02 22:16:55 +0100

    two sum

---
+++ Beginner/anagram.py
@@ -0,0 +1,7 @@
+# Given two strings s and t, return True if t is an anagram of s, and False otherwise.
+
+# 🧪 Examples:
+# python
+
+# Input: s = "listen", t = "silent" → Output: True
+# Input: s = "hello", t = "helloo" → Output: False
